a77cc723adeffc74989f4bd993081e374dd60310
Yrjö Peussa <peussa@iki.fi>
2014-11-05 16:57:35 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -23,29 +23,42 @@ $(function() {
 var heightMapWidth = 128;
 var heightMapLength = 128;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene, camera, renderer;
+var sandGeometry;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+init();
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
 
 var geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
 var material2 = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-
 var cube2 = new THREE.Mesh( geometry2, material2 );
-
-scene.add(cube);
-
 scene.add(cube2);
 
-camera.position.z = 5;
+render();
 
 function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.z = 5;
+  camera.position.y = 2;
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  sandGeometry = new THREE.PlaneGeometry(10, 10, heightMapWidth-1, heightMapLength-1);
+  sandGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+  sandGeometry.computeFaceNormals();
+  sandGeometry.computeVertexNormals();
+  var checkerBoardTexture = THREE.ImageUtils.loadTexture("images/white-gray-checkerboard.png");
+  checkerBoardTexture.wrapS = checkerBoardTexture.wrapT = THREE.RepeatWrapping;
+  checkerBoardTexture.repeat.set(5,5);
+  var sandMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe:true});
+  var mesh = new THREE.Mesh(sandGeometry, sandMaterial);
+  scene.add(mesh);
+  document.body.appendChild( renderer.domElement );
 }
 
 function render() {
@@ -56,5 +69,3 @@ function render() {
 	requestAnimationFrame( render );
 }
 
-init();
-render();
