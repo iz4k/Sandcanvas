5210bb269a61ff250dcee7da9b93abf9130579cf
iz4k <ilari.meuronen@gmail.com>
2014-11-18 14:40:24 +0200

    mouse click functionality extended to mouse drag. slider now controls maxheight.

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,8 @@ $(function() {
     	step:1,
     	slide:function (event, ui) {
     		$('.value').html(ui.value);
+        maxSandHeight = ui.value;
+        updateMesh();
     	}
     });
     // Shows default value
@@ -90,7 +92,17 @@ function init() {
   document.body.appendChild( renderer.domElement );
   window.addEventListener( 'resize', onWindowResize, false );
   
-  document.addEventListener("mousedown", onMouseDown, false);
+  document.addEventListener("mousedown", function(){
+    document.onmousemove = function(e){
+      onMouseDown(e);
+    }
+    this.onmouseup = function() {
+      document.onmousemove = null
+    }
+
+  }, false);
+
+  // document.addEventListener("drag", onMouseDown, false);
 }
 
 function render() {
@@ -151,7 +163,6 @@ function onMouseDown( event ) {
     
 
 }
-
 function heightMapPos(x,z){  //this is bad
  
  var magiaa = 12; 
